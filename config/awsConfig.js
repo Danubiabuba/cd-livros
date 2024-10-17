f7af59c7c6fe8a88e9ccc5b32e4c624cf2248c99
Danubiabuba <danubiaalves184@gmail.com>
2024-10-17 17:17:26 -0300

    Configuração do banco de dados e variáveis de ambiente

---
+++ config/awsConfig.js
@@ -0,0 +1,11 @@
+// config.js
+require('dotenv').config();
+
+module.exports = {
+    awsConfig: {
+        accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+        secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+        region: process.env.AWS_REGION
+    },
+    s3BucketName: process.env.S3_BUCKET_NAME
+};
