e55c08ce276e25fe4705f355b558fa52736ca050
Danubiabuba <danubiaalves184@gmail.com>
2024-10-21 10:32:32 -0300

    Adicionar validação para campos obrigatórios no formulário de criação de livros

--- routes/bookRoutes.js
+++ routes/bookRoutes.js
@@ -3,12 +3,21 @@ const multer = require('multer');
 const bookController = require('../controllers/bookController');
 const router = express.Router();
 const upload = multer({ storage: multer.memoryStorage() });
+const { body } = require('express-validator');
+
 
 // Rota para renderizar o formulário de criação de livro
 router.get('/create', (req, res) => res.render('createBook'));
 
 // Rota para criar um novo livro
-router.post('/create', upload.single('coverImage'), bookController.createBook);
+router.post('/create',
+    upload.single('coverImage'),
+    [
+        body('title').notEmpty().withMessage('Título é obrigatório.'),
+        body('author').notEmpty().withMessage('Autor é obrigatório.')
+    ],
+    bookController.createBook
+);
 
 // Rota para listar os livros
 router.get('/', bookController.listBooks);
